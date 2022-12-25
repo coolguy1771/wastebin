eac08107d51ca35029171885b5b2ae3905cc4d0f
Tyler Witlin <tyler@codezero.io>
2022-12-25 13:12:17 -0500

    feat(backend): allow support for shutdown signals
    Signed-off-by: Tyler Witlin <tyler@codezero.io>

--- cmd/wastebin/main.go
+++ cmd/wastebin/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/coolguy1771/wastebin/config"
 	"github.com/coolguy1771/wastebin/log"
 	"github.com/coolguy1771/wastebin/storage"
@@ -39,6 +43,19 @@ func main() {
 	routes.AddRoutes(app)
 
 	log.Info("Starting the server", zap.String("port", config.Conf.WebappPort))
+
+	// Create a channel to receive OS signals
+	sigChan := make(chan os.Signal, 1)
+
+	// Register the channel to receive SIGINT (Ctrl+C) and SIGTERM signals
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Use a separate goroutine to listen for signals and shutdown the server gracefully
+	go func() {
+		sig := <-sigChan
+		log.Info("Received signal to shutdown server", zap.String("signal", sig.String()))
+		app.Shutdown()
+	}()
 
 	// Listen on the user specified port defaulting to 3000
 	if err := app.Listen(":" + config.Conf.WebappPort); err != nil {
